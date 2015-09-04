c5960932182f1480646f4ede5d40db75d6bd50df
Kevin Han <k3vinhan@gmail.com>
2015-09-03 17:54:58 -0700

    1st Commit

---
+++ CrackingCode/Unique.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrackingCode
+{
+    public class Unique
+    {
+
+        public static bool unique(string s)
+        {
+            foreach (char c in s)
+            {
+                int count = s.Where(x => x == c).Count();
+                if (count > 1)
+                { return false; }
+            }
+            return true;
+        }
+    }
+}
---
+++ UnitTestCrackingCode/UnitTest1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CrackingCode;
+
+namespace UnitTestCrackingCode
+{
+    [TestClass]
+    public class UniqueTest
+    {
+        [TestMethod]
+        public void CheckTrueCases()
+        {
+            var uni=new Unique();
+            bool actual= Unique.unique("ABCDEFG");
+            Assert.AreEqual(true, actual);
+        }
+         [TestMethod]
+        public void CheckFalseCases()
+        {
+            var uni = new Unique();
+            bool actual = Unique.unique("ABCDEFGA");
+            Assert.AreEqual(false, actual);
+        }
+         [TestMethod]
+        public void CheckcaseFalseCases()
+        {
+            var uni = new Unique();
+            bool actual = Unique.unique("ABCDEFGa");
+            Assert.AreEqual(false, actual);
+        }
+
+    }
+}
